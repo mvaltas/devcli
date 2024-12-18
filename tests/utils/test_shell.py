b2443ac188a389d5796691d2eaa6de36b372a6ce
Marco Valtas <mavcunha@gmail.com>
2024-12-17 20:45:18 -0500

    Format fixing

--- tests/utils/test_shell.py
+++ tests/utils/test_shell.py
@@ -1,8 +1,10 @@
 import devcli.utils.shell as shell
 
+
 def _expand(obj: enumerate[str]):
     return {index: value for index, value in obj}
 
+
 def test_cmds_as_list_transformed_into_dict():
-    cmds = shell.iter_for(['cmd1', 'cmd2'])
-    assert _expand(enumerate(['cmd1', 'cmd2'])) == _expand(cmds)
+    cmds = shell.iter_for(["cmd1", "cmd2"])
+    assert _expand(enumerate(["cmd1", "cmd2"])) == _expand(cmds)
