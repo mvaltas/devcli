c52ecd38c01c6567b5dc28892591576d089d3a87
Marco Valtas <mavcunha@gmail.com>
2025-06-11 16:42:45 -0700

    Couple tests for op command example as accepting account param

--- devcli/commands/op.py
+++ devcli/commands/op.py
@@ -1,4 +1,5 @@
-from typer import Context
+from typing import Optional
+from typer import Context, Option
 
 import devcli.framework as cmd
 from devcli.framework.errors import MissConfError
@@ -7,32 +8,45 @@ from devcli.utils.one_password import OnePassword
 cli = cmd.new("Shortcuts for 1Password CLI")
 
 
-def get_op(config):
+def get_op(config, account: Optional[str] = None):
     # vault is optional and defaults to 'Private' on OnePassword
     vault = config["devcli.commands.op.vault"]
     if vault is None:
         vault = "Private"
 
-    # account is mandatory for op cli to work
-    acct = config["devcli.commands.op.account"]
-    if acct is None:
-        raise MissConfError(
-            topic="devcli.commands.op", entry="account", example="VALID_OP_ACCOUNT"
-        )
+    # account is mandatory and defaults to 'devcli.commands.op.accounts.default'
+    if account is not None:
+        acct = config[f"devcli.commands.op.accounts.{account}"]
+    else:
+        acct = config["devcli.commands.op.accounts.default"]
+        if acct is None:
+            raise MissConfError(
+                topic="devcli.commands.op.accounts",
+                entry="default",
+                example="VALID_OP_ACCOUNT",
+            )
     return OnePassword(acct, vault=vault)
 
 
 @cli.command()
-def credential(ctx: Context, key: str):
+def credential(
+    ctx: Context,
+    key: str,
+    account: Optional[str] = Option(None, "--account", help="1Password account to use"),
+):
     """
     Will read from 1Password an entry in the form op://Private/{key}/credential
     """
-    cmd.echo(get_op(ctx.obj).credential(key))
+    cmd.echo(get_op(ctx.obj, account).credential(key))
 
 
 @cli.command()
-def password(ctx: Context, item: str):
+def password(
+    ctx: Context,
+    item: str,
+    account: Optional[str] = Option(None, "--account", help="1Password account to use"),
+):
     """
     Will read from 1Password a login entry and return its password
     """
-    cmd.echo(get_op(ctx.obj).password(item))
+    cmd.echo(get_op(ctx.obj, account).password(item))
--- tests/commands/test_op.py
+++ tests/commands/test_op.py
@@ -1,1 +1,17 @@
+import pytest
 
+
+@pytest.fixture(autouse=True)
+def setup(devcli_cmd):
+    global op
+    op = devcli_cmd("op")
+
+
+def test_password():
+    result = op("password", "--account", "personal", "devcli-test")
+    assert result.output in "this-is-a-test-password\n"
+
+
+def test_credentials():
+    result = op("credential", "--account", "personal", "devcli-test-api")
+    assert result.output in "some-sensitive-api-value\n"
