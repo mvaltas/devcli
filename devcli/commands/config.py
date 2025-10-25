068e6de12d145f84f5da5ef2b8ff135028624068
Marco Valtas <mavcunha@gmail.com>
2025-10-25 11:32:06 -0700

    Implement a get all on config

--- devcli/commands/config.py
+++ devcli/commands/config.py
@@ -7,11 +7,19 @@ cli = cmd.new("Shortcuts to read from devcli configuration")
 
 @cli.command()
 def get(ctx: Context, key: str):
-    """Gets the value of a config using 'key'"""
+    """
+    Gets the value of a config using 'key'
+    """
     cmd.echo(ctx.obj[key])
 
 
 @cli.command()
 def get_all(ctx: Context):
-    """List all configurations parsed by devcli"""
+    """
+    List all configurations after parsing in TOML format
+
+    The configuration will not necessarily resemble the
+    original configuration files. Topics will be shown
+    until the (n - 1) key, follow by their values.
+    """
     cmd.echo(ctx.obj)
